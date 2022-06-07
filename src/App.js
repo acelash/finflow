10fc7eba0f57b95830064b03cbc1b66f5b93a825
Andrei Gudima <andrei.gudima@sendmoments.de>
2022-06-07 17:22:29 +0300

    added router, mui template pack and some demo pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
     </div>
   );
+}
+
+function Home() {
+    return (
+        <>
+            <main>
+                <h2>Welcome to Finflow!</h2>
+                <p>You can do this, I believe in you.</p>
+            </main>
+            <nav>
+                <Link to="/login">Login</Link>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/about">About</Link>
+            </nav>
+        </>
+    );
+}
+
+function About() {
+    return (
+        <>
+            <main>
+                <h2>Who are we?</h2>
+                <p>
+                    That feels like an existential question, don't you
+                    think?
+                </p>
+            </main>
+            <nav>
+                <Link to="/">Home</Link>
+            </nav>
+        </>
+    );
+}
+function NotFound() {
+    return (
+        <>
+            <main>
+                <h2>404</h2>
+                <p>
+                    The page was not found
+                </p>
+            </main>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="about">About</Link>
+            </nav>
+        </>
+    );
 }
 
 export default App;
